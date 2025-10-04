3a317cfa1eacbbc06bea2e8b863171998bd5a641
rumky9963 <annikaislam804@gmail.com>
2025-10-04 12:53:47 +0600

    update data

--- app.py
+++ app.py
@@ -11,6 +11,10 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
 @app.route('/prediction', methods=['GET', 'POST'])
 def prediction():
     if request.method == "POST":
